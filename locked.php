5ee8a3159fa1b6691c3d2e2b428a1697c1096736
Rebecca Snyder <rsnyder@clickhere.com>
2013-11-22 16:47:02 -0600

    initial push

---
+++ locked.php
@@ -0,0 +1,135 @@
+<?php include("/home2/melimbac/public_html/password_protect.php"); ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]> <html class="no-js" lang="en"> <![endif]-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+
+   <title>Melimba | Locked Patterns</title>
+	<meta name="keywords" content="melimba, beccabury, fabric, art, prints, stationery, cards, patterns, Riley Blake, Kokka, designer, Texas, Utah">
+	<link rel="shortcut icon" href="/favicon.ico">
+	<link rel="stylesheet" href="_css/reset.css" type="text/css" media="all" title="global" charset="utf-8" />
+	<link rel="stylesheet" href="_css/style.css" type="text/css" media="all" title="global" charset="utf-8" />
+	<link type="text/plain" rel="author" href="humans.txt" />
+  
+  <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  
+   <!--Google Analytics Tracking-->
+<script type="text/javascript"> 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33625803-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+  
+</head>
+
+<body>
+  <div id="wrapper">
+  <div id="header">
+    <a id="logo" href="custom.html"><img src="_images/melimba.png" width="412" height="64" alt="Melimba"></a>
+  </div>
+  
+  <div class="clearer"></div>
+  
+  <div id="content">
+    <div id="nav" class="column-1">
+      <ul id="nav">
+        <li><a href="custom.html">Custom</a></li>
+        <li><a href="prints.html">Prints</a></li>
+        <li><a href="cards.html">Cards</a></li>
+        <li><a href="patterns.html">Patterns</a></li>
+        <li><a href="random.html">Random</a></li>
+        <li><a href="about.html">About</a></li>
+      </ul>
+    </div>
+    
+    <div id="one" class="column">
+      <img src="_images/patterns/garden/_garden-colorway-1-pg2.png" alt="Garden Colorway 1 Pg2"><!--FIX-->
+      <img src="_images/patterns/garden/_garden-colorway-2-pg1.png" alt="Garden Colorway 2 Pg1"><!--FIX-->
+      <img src="_images/patterns/world/anchors.png" alt="Anchors">
+      <img src="_images/patterns/world/birds.png" alt="Birds">
+      <img src="_images/patterns/world/meWantHoneyComb.png" alt="MeWantHoneyComb">
+      <img src="_images/patterns/world/ships.png" alt="Ships">
+      <img src="_images/patterns/dinosaurs/_dinosaur-11x17-press-ONE-page.png" alt="Dinosaur 11x17 Press ONE Page">
+      <img src="_images/patterns/house/blueHouses.png" alt="BlueHouses">
+      <img src="_images/patterns/house/neighborhood.png" alt="Neighborhood">
+      <img src="_images/patterns/games/pacman.png" alt="Pacman">
+      <img src="_images/patterns/cars/_cartalkPAGE2.png" alt="CartalkPAGE2">
+      <img src="_images/patterns/space/planets.png" alt="Planets">
+      <img src="_images/patterns/space/SpaceCadets-colorway-1-pg1.png" alt="SpaceCadets Colorway 1 Pg1">
+      <img src="_images/patterns/space/SpaceCadets-colorway-1-pg2.png" alt="SpaceCadets Colorway 1 Pg2">
+      <img src="_images/patterns/sports/white.png" alt="White"><!--FIX-->
+      <img src="_images/patterns/circus/total.png" alt="Total">
+      <img src="_images/patterns/circus/brownCollage.png" alt="BrownCollage">
+      <img src="_images/patterns/circus/blueFlags.png"  alt="BlueFlags">
+      <img src="_images/patterns/circus/pinkish.png" alt="Pinkish">
+      <img src="_images/patterns/circus/tickets.png" alt="Tickets">
+      <img src="_images/patterns/party/PartyPRESS-11x17-to-PRINT.png" alt="PartyPRESS 11x17 To PRINT">
+      <img src="_images/patterns/party/fourPatterns.png" alt="FourPatterns">
+      <img src="_images/patterns/party/party-zoomed-2.png" alt="Party Zoomed 2">
+      <img src="_images/patterns/party/party-zoomed-1.png" alt="Party Zoomed 1">
+      <img src="_images/patterns/party/party-zoomed-3.png" alt="Party Zoomed 3">
+      <img src="_images/patterns/party/blueFloral.png" alt="BlueFloral">
+      <img src="_images/patterns/party/comboCake.png"  alt="ComboCake">
+      <img src="_images/patterns/cuckoo/4square.png" alt="4square">
+      <img src="_images/patterns/cuckoo/acornKids.png" alt="AcornKids">
+      <img src="_images/patterns/cuckoo/blueDamask.png"  alt="BlueDamask">
+      <img src="_images/patterns/cuckoo/CuckooCollage.png" alt="CuckooCollage">
+      <img src="_images/patterns/cuckoo/greyLeaves.png" alt="GreyLeaves">
+      <img src="_images/patterns/cuckoo/greenDaisy.png" alt="GreenDaisy">
+       <div class="paragraph">
+          <p class="copyright">© 2012 melimba&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;all rights reserved</p>
+       </div><!--end .paragraph-->
+    </div><!--end #one-->
+    
+    
+    <div id="two" class="column last">
+      <img src="_images/patterns/garden/_garden-colorway1.png" alt="Garden Colorway1"><!--FIX-->
+      <img src="_images/patterns/garden/_garden-colorway-2-pg2.png" alt="Garden Colorway 2 Pg2">
+      <img src="_images/patterns/world/africa.png" alt="Africa">
+      <img src="_images/patterns/world/_blueWorld.png" alt="BlueWorld">
+      <img src="_images/patterns/world/blue_ships.png" alt="Blue Ships">
+      <img src="_images/patterns/world/polka_stripes.png" alt="Polka Stripes">
+      <img src="_images/patterns/world/white_world.png" alt="White World">
+      <img src="_images/patterns/world/yellow_plaid.png" alt="Yellow Plaid">
+      <img src="_images/patterns/dinosaurs/stripe.png"  alt="Stripe">
+      <img src="_images/patterns/dinosaurs/brown.png" alt="Brown">
+      <img src="_images/patterns/games/tetris.png" alt="Tetris">
+      <img src="_images/patterns/cars/_cartalkPAGE1.png" alt="CartalkPAGE1">
+      <img src="_images/patterns/cars/_cartalkPAGE3.png" alt="CartalkPAGE3">
+      <img src="_images/patterns/space/SpaceCadets-colorway-2-pg1.png" alt="SpaceCadets Colorway 2 Pg1">
+      <img src="_images/patterns/space/SpaceCadets-colorway-2-pg2.png" alt="SpaceCadets Colorway 2 Pg2">
+      <img src="_images/patterns/sports/orange.png" alt="Orange">
+      <img src="_images/patterns/sports/white_collage.png" alt="White Collage"> <!--FIX-->
+      <img src="_images/patterns/circus/collage.png"  alt="Collage">
+      <img src="_images/patterns/circus/flags.png" alt="Flags">
+      <img src="_images/patterns/circus/pinkCollage.png" alt="PinkCollage">
+      <img src="_images/patterns/circus/goldenTicket.png" alt="GoldenTicket">
+      <img src="_images/patterns/circus/beach_balls.png" alt="Beach Balls">
+      <img src="_images/patterns/party/goldenCakes.png" alt="GoldenCakes">
+      <img src="_images/patterns/party/pluses.png">
+      <img src="_images/patterns/party/specialPlates.png">
+      <img src="_images/patterns/party/party-zoomed-4.png" alt="Party Zoomed 4">
+      <img src="_images/patterns/party/party-zoomed-5.png" alt="Party Zoomed 5">
+      <img src="_images/patterns/party/party-zoomed-6.png" alt="Party Zoomed 6">
+      <img src="_images/patterns/party/party-zoomed-presents.png" alt="Party Zoomed Presents">
+      <img src="_images/patterns/cuckoo/total.png"  alt="Total">
+      <img src="_images/patterns/cuckoo/blueCuckoo.png" alt="BlueCuckoo">
+      <img src="_images/patterns/cuckoo/daisies.png" alt="Daisies">
+      <img src="_images/patterns/cuckoo/blueXs.png" alt="BlueXs">
+      <img src="_images/patterns/cuckoo/greenDamask.png" alt="GreenDamask">
+      <img src="_images/patterns/cuckoo/yellowXs.png" alt="YellowXs">
+    </div><!--end #two-->
+  </div><!--end #content-->
+  </div><!--end #wrapper-->
+</body>
+</html>
